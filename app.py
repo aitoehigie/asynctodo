e889071bc83b9079d8e6cd33521f6abf9b54b401
aitoehigie <aitoehigie@gmail.com>
2017-10-29 21:58:24 +0100

    Add get_one_todo method and app_factory

--- app.py
+++ app.py
@@ -15,7 +15,21 @@ async def get_all_todos(request):
         {'id': idx, **todo} for idx, todo in enumerate(TODOS)
     ])
 
-app = web.Application()
-app.router.add_get("/", get_all_todos)
+async def get_one_todo(request):
+    id = int(request.match_info["id"])
+    if id >= len(TODOS):
+        return web.json_response({"error": "Todo not found"}, status=404)
+    return web.json_response({"id":id, **TODOS[id]})
 
-web.run_app(app)
+
+
+def app_factory(args=()):
+    app = web.Application()
+    app.router.add_get("/todos/", get_all_todos)
+    app.router.add_get('/todos/{id:\d+}', get_one_todo, name='one_todo')
+    return web.run_app(app)
+
+if __name__ == "__main__":
+    app_factory()
+
+#web.run_app(app)
